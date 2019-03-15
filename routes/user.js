5c90d1524f3eab89ea1e2662c8ab1fc77dd1f8a7
Ussama Azam <nuclearoreo@gmail.com>
2019-03-14 20:21:41 -0400

    More fixes

--- routes/user.js
+++ routes/user.js
@@ -7,11 +7,13 @@ const {User, userValidation} = require('../models/user');
 
 router.get('/', async (req,res) => {
     let users;
-    if (req.body.email) {
-        users = await User.find({ email: req.body.email });
-    } else {
-        users = await User.find();
+    try {
+        if (req.body.email) users = await User.find({ email: req.body.email });
+        else users = await User.find(); 
+    } catch (ex) {
+        return res.status(500).send(ex);
     }
+
     const result = _.map(users, _.partialRight(_.pick, ['_id', 'email']));
     res.send(result);
 });
@@ -21,23 +23,33 @@ router.post('/',  async (req, res) => {
     if (error) return res.status(400).send(error.details[0].message);
 
     let user = await User.findOne({email: req.body.email});
-    if (user) return res.status(400).send('Email already exists');
+    if (user) return res.status(409).send('Email already exists');
 
     user = new User(_.pick(req.body, ['email', 'password']));
     const salt = await bcrypt.genSalt(10);
     user.password = await bcrypt.hash(user.password, salt);
 
-    await user.save();
-    res.send(user.genToken());
+    try {
+        await user.save();
+        res.send(user.genToken());
+    } catch (ex) {
+        return res.status(500).send(ex);
+    }
 });
 
 router.delete('/', async (req, res) => {
     const token = req.header('x-auth-token');
     if (!token) return res.status(400).send('No auth token');
+
     const decoded = jwt.decode(token, 'secret');
     if (!decoded) return res.status(400).send('Bad token'); 
-    const result = await User.deleteOne({ id: decoded.id });
-    res.send(result);
+
+    try {
+        const result = await User.deleteOne({ _id: decoded.id });
+        res.send(result);
+    } catch (ex) {
+        return res.status(500).send(ex); 
+    }
 });
 
 module.exports = router;
