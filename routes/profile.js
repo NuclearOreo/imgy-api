6f6a460c2e05b6b761acebec72eba5f18df7c7d8
Ussama Azam <nuclearoreo@gmail.com>
2019-03-18 17:01:36 -0400

    Added get request for profile

--- routes/profile.js
+++ routes/profile.js
@@ -2,3 +2,20 @@ const _ = require('lodash');
 const app = require('express');
 const router =  app.Router();
 const {Profile, profileValidation} = require('../models/profile');
+
+router.get('/', async (req,res) => {
+    let profiles;
+    
+    try {
+        if (req.body.email) profiles = await Profile.find({ 
+            _id: req.body.id, firstname: req.body.firstname, lastname: req.body.lastname
+        });
+        else profiles= await Profile.find(); 
+    } catch (ex) {
+        return res.status(500).send(ex);
+    }
+
+    res.send(profiles);
+});
+
+module.exports = router;
