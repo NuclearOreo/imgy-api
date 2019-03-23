e046ac559d92d5f8ecdbe3f298525a781f0f77f6
Ussama Azam <nuclearoreo@gmail.com>
2019-03-22 22:15:28 -0400

    Removing post in catch blocks

--- routes/post.js
+++ routes/post.js
@@ -3,71 +3,51 @@ const app = require('express');
 const router = app.Router();
 const {Post, postValidation} = require('../models/post');
 const auth = require('../middleware/auth');
+const asyncMiddleware = require('../middleware/async');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
 
-router.get('/', async (req,res) => {
-    try {
-        const posts = await Post.find();
-        res.send(posts);
-    } catch(ex) {
-        res.status(500).send(ex);
-    }
-});
+router.get('/', asyncMiddleware(async (req,res) => {
+    const posts = await Post.find();
+    res.send(posts);
+}));
 
-router.get('/:username', async (req,res) => {
-    try {
-        const posts = await Post.find({ username: req.params.username });
-        res.send(posts);
-    } catch(ex) {
-        res.status(500).send(ex);
-    }
-});
+router.get('/:username', asyncMiddleware(async (req,res) => {
+    const posts = await Post.find({ username: req.params.username });
+    res.send(posts);
+}));
 
-router.get('/id/:id', async (req,res) => {
+router.get('/id/:id', asyncMiddleware(async (req,res) => {
     const {error} = Joi.validate(req.params, { id: Joi.objectId() });
     if (error) return res.status(400).send(error.details[0].message);
 
-    try {
-        const post = await Post.findOne({ _id: req.params.id });
-        res.send(post);
-    } catch(ex) {
-        res.status(500).send(ex);
-    }
-});
+    const post = await Post.findOne({ _id: req.params.id });
+    res.send(post);
+}));
 
-router.post('/:username', auth, async (req,res) => {
+router.post('/:username', auth, asyncMiddleware(async (req,res) => {
     if (req.params.username !== req.body.username) return res.status(400).send('Not Authorized');
 
     const newpost = _.pick(req.body, ['imageUrl', 'username']);
     const {error} = postValidation(newpost);
     if (error) return res.status(400).send(error.details[0].message);
 
-    try {
-        const post = new Post(newpost);
-        await post.save();
-        res.send(post);
-    } catch (ex) {
-        res.status(500).send(ex);
-    }
-});
+    const post = new Post(newpost);
+    await post.save();
+    res.send(post);
+}));
 
-router.delete('/:id', auth, async (req,res) => {
+router.delete('/:id', auth, asyncMiddleware(async (req,res) => {
     const {error} = Joi.validate(req.params, { id: Joi.objectId() });
     if (error) return res.status(400).send(error.details[0].message);
-    
-    try {
 
-        const post = await Post.findById(req.params.id);
-        if (!post) return res.status(400).send('Post does not exist');
+    const post = await Post.findById(req.params.id);
+    if (!post) return res.status(400).send('Post does not exist');
 
-        if (post.username !== req.body.username) return res.status(400).send('Not authorized to delete');
+    if (post.username !== req.body.username) return res.status(400).send('Not authorized to delete');
 
-        const result = await Post.deleteOne({ _id: req.params.id });
-        res.send(result);
-    } catch (ex) {
-        res.status(500).send(ex);
-    }
-});
+    const result = await Post.deleteOne({ _id: req.params.id });
+    res.send(result);
+}));
 
 module.exports = router;
