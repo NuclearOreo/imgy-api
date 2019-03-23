2ecb8a055120ea71c239fb55c5035bac0f46d89b
Ussama Azam <nuclearoreo@gmail.com>
2019-03-22 22:11:55 -0400

    Removed the catch block in profiles routes

--- routes/profile.js
+++ routes/profile.js
@@ -3,58 +3,39 @@ const app = require('express');
 const router =  app.Router();
 const {Profile, profileValidation} = require('../models/profile');
 const auth = require('../middleware/auth');
+const asyncMiddleware = require('../middleware/async');
 
-router.get('/', async (req,res) => {
-    try {
-        const profiles = await Profile.find();
-        res.send(profiles);
-    } catch (ex) {
-        res.status(500).send(ex);
-    }
-});
+router.get('/', asyncMiddleware(async (req,res) => {
+    const profiles = await Profile.find();
+    res.send(profiles);
+}));
 
-router.get('/:username', async (req,res) => {
-    try {
-        const profile = await Profile.findOne({ username: req.params.username });
-        res.send(profile);
-    } catch (ex) {
-        res.status(500).send(ex);
-    }
-});
+router.get('/:username', asyncMiddleware(async (req,res) => {
+    const profile = await Profile.findOne({ username: req.params.username });
+    res.send(profile);
+}));
 
-router.post('/', auth, async (req, res) => {
+router.post('/', auth, asyncMiddleware( async (req, res) => {
     const {error} = profileValidation(_.pick(req.body, ['userId', 'username', 'firstname', 'lastname', 'street', 'city', 'zip']));
     if (error) return res.status(400).send(error.details[0].message);
 
-    try {
-        const profile = await Profile.findOne({ username: req.body.username });
-        if (profile) return res.status(409).send('Profile exists');
-        newprofile = new Profile(_.pick(req.body, ['userId', 'username', 'firstname', 'lastname', 'street', 'city', 'zip']));
-        await newprofile.save();
-        res.send(newprofile);
-    } catch (ex) {
-        res.status(500).send(ex);
-    }
-});
+    const profile = await Profile.findOne({ username: req.body.username });
+    if (profile) return res.status(409).send('Profile exists');
+    newprofile = new Profile(_.pick(req.body, ['userId', 'username', 'firstname', 'lastname', 'street', 'city', 'zip']));
+    await newprofile.save();
+    res.send(newprofile);
+}));
 
-router.put('/', auth, async (req, res) => {
-    try {
-        const result = await Profile.findOneAndUpdate({ userId: req.body.userId }, _.pick(req.body,[
-            'firstname', 'lastname', 'street', 'city', 'zip'
-        ]));
-        res.send(result);
-    } catch (ex) {
-        res.status(500).send(ex);
-    }
-});
+router.put('/', auth, asyncMiddleware(async (req, res) => {
+    const result = await Profile.findOneAndUpdate({ userId: req.body.userId }, _.pick(req.body,[
+        'firstname', 'lastname', 'street', 'city', 'zip'
+    ]));
+    res.send(result);
+}));
 
-router.delete('/', auth, async (req, res) => {
-    try {
-        const result = await Profile.deleteOne({ userId: req.body.userId });
-        res.send(result);
-    } catch (ex) {
-        res.status(500).send(ex);
-    }
-});
+router.delete('/', auth, asyncMiddleware(async (req, res) => {
+    const result = await Profile.deleteOne({ userId: req.body.userId });
+    res.send(result);
+}));
 
 module.exports = router;
