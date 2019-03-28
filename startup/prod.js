008112126cd34f1f771c7b274338f46da9b9465b
Ussama Azam <nuclearoreo@gmail.com>
2019-03-27 23:18:13 -0400

    Trying something

--- startup/prod.js
+++ startup/prod.js
@@ -1,9 +1,14 @@
 const helmet = require('helmet');
 const compression = require('compression');
-const cors = require('cors');
 
 module.exports = function (app) {
     app.use(helmet());
     app.use(compression());
-    app.use(cors());
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", '*');
+        res.header("Access-Control-Allow-Credentials", true);
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+        res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+        next();
+    });
 }   
