f0cabe7b2e3e76c4b87d399bba9ede0f0861009f
Ussama Azam <nuclearoreo@gmail.com>
2019-03-27 23:34:44 -0400

    Another attempt

--- startup/prod.js
+++ startup/prod.js
@@ -4,11 +4,4 @@ const compression = require('compression');
 module.exports = function (app) {
     app.use(helmet());
     app.use(compression());
-    app.use(function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", '*');
-        res.header("Access-Control-Allow-Credentials", true);
-        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-        res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-        next();
-    });
 }   
